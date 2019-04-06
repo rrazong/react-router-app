20ca7be8118be0527e09601326f2d7ccee799394
Rich Razon <rich@carrot.co>
2019-04-06 09:04:56 -0700

    Uses withRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Redirect, withRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -21,6 +21,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <h2>Created by Router</h2>
           <ul>
             {
               routes.map(({path, name}) => (
@@ -39,6 +40,13 @@ class App extends Component {
               );
             })
           }
+          <hr/>
+          <h2>Not created by Router</h2>
+          <span>So, this.props.history does not exist</span>
+          <AccessCodes getInitialData={() => {console.log('Get data for access codes')}}/>
+          <hr/>
+          <h2>Created by withRouter</h2>
+          <BetterAccessCodes getInitialData={() => {console.log('Get data for better access codes')}}/>
         </div>
       </Router>
     );
@@ -46,14 +54,27 @@ class App extends Component {
 }
 
 class Complaints extends Component {
+  state = {
+    goToAccessCodes: false,
+  }
+
   componentWillMount() {
     this.props.getInitialData();
   }
 
+  handleClickToAccessCodes = () => {
+    this.setState({goToAccessCodes: true})
+  }
+
   render() {
+    console.log('state is', this.state);
     return (
       <div>
+        { this.state.goToAccessCodes &&
+          <Redirect to='/access-codes'/>
+        }
         Complaints module
+        <button onClick={this.handleClickToAccessCodes}>Go to Access Codes</button>
       </div>
     );
   }
@@ -64,13 +85,20 @@ class AccessCodes extends Component {
     this.props.getInitialData();
   }
 
+  handleClickToComplaints = () => {
+    this.props.history.push('/complaints');
+  }
+
   render() {
     return (
       <div>
         Access Codes module
+        <button onClick={this.handleClickToComplaints}>Go to Complaints</button>
       </div>
     );
   }
 }
+
+const BetterAccessCodes = withRouter(AccessCodes);
 
 export default App;
