c03f47572fb861819cf0a4babf4b362c917bfc16
Rich Razon <rich@carrot.co>
2019-04-06 10:34:53 -0700

    Passing state through a Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -8,19 +8,18 @@ class App extends Component {
         <div className="App">
           <ul>
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/page-exists'>Page exists</Link></li>
-            <li><Link to='/deprecated-page'>Deprecated page, redirects to an existing page</Link></li>
-            <li><Link to='/user/123'>User page got user 123</Link></li>
-            <li><Link to='/page-does-not-exist'>Page does not exist</Link></li>
-            <li><Link to='/also/does/not/exist'>Page also does not exist</Link></li>
+            <li><Link to={{
+              pathname: '/user/123',
+              state: {
+                from: 'home',
+                trackingId: 'abcd-234',
+              }
+            }}>User page got user 123</Link></li>
           </ul>
 
           <Switch>
             <Route exact path='/' component={Home}/>
-            <Route path='/page-exists' component={PageExists}/>
             <Route path='/user/:userId' component={User}/>
-            <Redirect from='/deprecated-page' to='/page-exists'/>
-            <Route component={PageNotFound}/>
           </Switch>
         </div>
       </Router>
@@ -29,17 +28,12 @@ class App extends Component {
 }
 
 const Home = () => (<div>Home</div>);
-const PageExists = () => (<div>Page OK</div>);
-const User = (props) => (<div>User Page - id {props.match.params.userId}</div>);
-
-class PageNotFound extends Component {
-  render() {
-    return (
-      <div>
-        Page Not Found: {this.props.location.pathname}
-      </div>
-    )
-  }
-}
+const User = (props) => (
+  <div>
+    <h1>User Page - id {props.match.params.userId}</h1>
+    <p>from {props.location.state.from}</p>
+    <p>tracking id {props.location.state.trackingId}</p>
+  </div>
+);
 
 export default App;
